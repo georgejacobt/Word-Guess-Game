fa5ce53e7bac845a555495faf5a441820c831ebf
George Jacob Work <36488006+georgejacobt@users.noreply.github.com>
2018-05-13 03:46:14 -0500

    revisions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 
 var wordsList =[];
 
+wordsList.push("")
+
 
 //var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
@@ -44,16 +46,37 @@ document.getElementById("wordBlank").innerHTML = drawDash;
 var guessArray =[];
 
 
-function guessXtract() {
-  if (event.keyCode >= 65 && event.keyCode <=122 ){
-     var guessLetter = event.key;
-    guessArray.push(guessLetter);
+//function guessXtract() {
     
-    for (let i =0; i < wordLength; i++){
+  document.onkeypress=function(e){
+    begin();
+}
+  
+document.onkeyup = function(begin){
+
+var guessLetter = event.key.toLowerCase();
+
+/*var fruit =["a"]
+     var price = [1.25, 1.50]
+
+     const indexOfA = fruit.indexOf("a")
+     const priceOfA = price[indexOfA]*/
+
+
+
+
+}
+
+  
+  if (event.keyCode >= 65 && event.keyCode <=122 ){
       
+     
+    guessArray.push(guessLetter);    
+    for (let i =0; i < wordLength; i++){
       if (guessLetter === wordArray[i]) {
          console.log("Great guessing");
          dashes.splice(i,1,guessLetter+' ');
+         
       }
       
     }
@@ -62,6 +85,29 @@ function guessXtract() {
         sum = sum + dashes[i];
     
    console.log(sum);
+ 
+
+
    document.getElementById("wordBlank").innerHTML = sum;
+   
 }
-}
+
+var input = document.getElementById("guess");
+let tries =12;
+input.addEventListener("keyup", function () {
+      tries = tries -1;  
+      document.getElementById("numberTries").innerHTML = tries;
+    
+    
+
+});
+
+//}
+
+/*var fruit =["a"]
+     var price = [1.25, 1.50]
+
+     const indexOfA = fruit.indexOf("a")
+     const priceOfA = price[indexOfA]*/
+
+
---
+++ assets/javascript/game1.js
@@ -0,0 +1,135 @@
+
+function pickWord (){
+let wordsArray =["tyrannosaurusRex",
+                "velociraptor",
+                "triceratops",
+                "apatosaurus",
+                "diplodocus",
+                "brachiosaurus",
+                "carnotaurus",
+                "stegosaurus",
+                "spinosaurus",
+                "ankylosaurus",
+                "iguanodon",
+                "allosaurus",
+                "parasaurolophus",
+                "deinonychus" ,
+                "titanosaurs"];
+
+var computerPick = wordsArray[Math.floor(Math.random() * wordsArray.length)];      
+return computerPick;  
+
+//let wordLength = computerPick.length ;
+
+
+
+}
+
+let word = pickWord();  
+console.log(word);
+let wordLength = word.length;
+console.log(wordLength);
+let wordToArray = [];
+let win =0;
+let check = "_"+" ";
+let complete = null;
+
+
+for (let i=0; i < wordLength; i++){
+    wordToArray.push(word.charAt(i));
+}
+
+let dashes = [];
+  let drawDash = "_"+" " ;
+
+
+for (let i=0; i < wordLength ; i++)
+dashes.push("_"+" ");
+
+let guesses = 13;
+
+function presentDashes (){
+  
+let sum ='';
+for (let i =0; i < wordLength; i++)
+sum = sum + dashes[i];
+
+document.getElementById("wordBlank").innerHTML = sum;
+return sum;
+}
+
+//document.onkeypress=function(e){
+    
+//}
+
+var kick = 0;let flag = 0;
+document.onkeyup = function(start){
+    
+   if (kick===0){
+       presentDashes();
+       document.getElementById("press").innerHTML = ''; 
+       document.getElementById("tries").innerHTML = 'Guesses Remaining:'+' '+ guesses; 
+       document.getElementById("win").innerHTML = "Win Count:"+" "+ win;
+       }
+       kick = 1;
+
+   //console.log(kick);
+   if (guesses >= 1){
+   let guessLetter = event.key.toLowerCase();
+   guesses = guesses -1;
+   //console.log(word);
+   console.log(guessLetter);
+   //console.log(wordLength);
+    
+
+  for (let i=0; i < wordLength; i++){
+       //console.log("here");
+       if( guessLetter === wordToArray[i]){
+          console.log("Great Guess");
+          dashes.splice(i,1,guessLetter+' ');
+          
+          
+       }
+    }
+    for (let i=0; i < wordLength; i++){
+        if (flag === dashes[i])
+           complete = false;
+        else complete = true;
+    }
+
+   if (complete === true) addWin();
+
+   let sumCurrent ='';
+    for (let i=0; i < wordLength; i++)
+        sumCurrent = sumCurrent + dashes[i];
+
+    console.log(sumCurrent);
+    document.getElementById("wordBlank").innerHTML = sumCurrent;
+    document.getElementById("tries").innerHTML = 'Guesses Remaining:'+' '+ guesses; 
+}
+else { 
+    document.getElementById("press").innerHTML = "You have reached maximum guesses";
+}
+}   
+
+function addWin(){
+win - win +1;
+
+document.getElementById("win").innerHTML = "Win Count"+" "+ win;
+
+}
+
+
+    
+ 
+
+    
+
+   
+
+
+
+
+
+
+
